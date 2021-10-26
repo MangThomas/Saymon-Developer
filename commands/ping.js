d9492b003979456c865cc8a9739331d9484c95fa
MangThomas <93190704+MangThomas@users.noreply.github.com>
2021-10-26 18:41:02 +0800

    Delete ping.js

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-if(cmd === `${prefix}ping`) {
-message. channel. send("Pinging..."). then(m =>{
-var ping = m. createdTimestamp - message. createdTimestamp;
-var botPing = Math. round(bot. pi);
-m. edit(`**:ping_pong: Pong! Your Ping Is:-**\n ${ping}ms`);
-});
-}
