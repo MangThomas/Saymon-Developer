9765f66977dc71c6e8607a21fb5be5cb5103aa27
MangThomas <93190704+MangThomas@users.noreply.github.com>
2021-10-26 21:06:08 +0800

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+require('dotenv').config();
+const bot = new Discord.Client();
+const token = 'config.token';
+
+bot.commands = new Discord.Collection();
+bot.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(bot, Discord);
+})
+
+bot.on('ready', () => {
+
+    bot.user.setActivity('+help', { type: "LISTENING" }).catch(console.error);
+    this.bot.user.setStatus('online');
+})
+
+bot.login(process.env.YOUR_TOKEN);
