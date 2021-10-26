d8b50dec25cc8530db160beffa84ab9aad5911da
MangThomas <93190704+MangThomas@users.noreply.github.com>
2021-10-26 18:10:38 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("index.js has been executed");
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("The bot is now online! Powered by Beginners");
+});
+
+client.on("message", (message) => {
+  if (message.author.bot) return;
+});
+
+client.login("YOUR_TOKEN");
