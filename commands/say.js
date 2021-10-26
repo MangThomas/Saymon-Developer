023906ae72d0c1ccf611d2bf8e6abda3eb7844e2
MangThomas <93190704+MangThomas@users.noreply.github.com>
2021-10-26 20:58:57 +0800

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,12 @@
+const prefix = `config.token`
+if (message.content.toLowerCase().startsWith(prefix + "say")) 
+ {
+  let MSG = message.content.split(" ");
+  let Query = MSG.slice(1).join("+");
+  let QueryD = MSG.slice(1).join(" ");
+  if (!Query) message.reply("Please specify something for me to say!")
+  else
+  {
+    message.channel.send(QueryD + " -" + message.author.tag)
+}
+}
